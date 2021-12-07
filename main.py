fc19d4e7742200b552c9584d63245f71d6752329
LollipopKit <2036293523@qq.com>
2021-12-07 10:17:18 +0800

    更新思修

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ subject_map = {
     'clang2': {'name': 'C语言下', 'func': not_implement},
     'junli': {'name': '军理', 'func': junli},
     'jinxiandaishi': {'name': '近代史', 'func': jindaishi},
-    'sixiu': {'name': '思修', 'func': not_implement},
+    'sixiu': {'name': '思修', 'func': maogai},
     'makesi': {'name': '马克思', 'func': maogai},
     'feedback': {'name': 'Q&A', 'func': maogai},
 }
