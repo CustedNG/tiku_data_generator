1e5454358af28492942fc3ddc35a85d6e5eca0d3
lollipopkit <lollipopkit@icloud.com>
2023-11-08 10:24:22 +0800

    大量修复习思想

--- const.py
+++ const.py
@@ -24,4 +24,4 @@ answer_convert_map = {
 convert_result_dir = 'convert/'
 
 # 支持本生成器生成的科目，并非旧题库留下的数据的科目
-full_support_subject = ['maogai', 'makesi', 'junli', 'jinxiandaishi', 'sixiu']
+full_support_subject = ['maogai', 'makesi', 'junli', 'jinxiandaishi', 'sixiu', 'xi']
